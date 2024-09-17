b0943989f9e64445f4f68c6154bd36d7710ffc22
YunussEmree <senyigityunusemre@gmail.com>
2024-09-17 12:49:05 +0300

    Solution for LeetCode 1688

---
+++ 1688-Count_Of_Matches/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution(6));
+    }
+    public static int Solution(int n){
+        return n-1;
+    }
+}
